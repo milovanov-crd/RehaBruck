a1c410313701e476d37d99f86e56f616cdbacbb5
Dejan Milovanov <milovanov@codered.digital>
2021-06-19 18:55:26 +0200

    Menu
    Added menu to be inserted into the project

---
+++ script.js
@@ -0,0 +1,36 @@
+var menu = document.getElementById('mobile-only-menu');
+var close = document.getElementById('close');
+var menuWindow = document.getElementById('modal');
+var header = document.getElementById('header');
+
+menu.addEventListener('click', showMenu);
+close.addEventListener('click', hideMenu);
+window.addEventListener('scroll', changeHeader);
+
+function showMenu()
+{
+    menuWindow.style.opacity = 1;
+    menuWindow.style.zIndex = 3;
+}
+
+function hideMenu()
+{
+    menuWindow.style.opacity = 0;
+    menuWindow.style.zIndex = -3;
+}
+
+function changeHeader()
+{
+    var scroll = this.scrollY;
+
+    if (scroll > 150)
+    {
+        header.style.backgroundColor = 'white';
+        header.style.padding = '1.875rem 0';
+    }
+        else
+        {
+            header.style.backgroundColor = 'transparent';
+            header.style.padding = '3.75rem 0';
+        }
+}
